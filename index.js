2fa77f360482efdfd412e382dc84f791c981d4b0
Guillaume ARM <trapcodien@gmail.com>
2021-01-23 18:13:20 +0100

    chore: save work in index.js file

--- index.js
+++ index.js
@@ -1,7 +1,108 @@
-const easymidi = require('easymidi');
-const usb = require('usb');
+const Canvas = require('canvas');
+const {
+  initPush,
+  sendFrame,
+} = require('@guillaumearm/ableton-push-canvas-display');
+const { Push2 } = require('@guillaumearm/ableton-push2');
 
-const ABLETON_VENDOR_ID = 0x2982;
-const PUSH2_PRODUCT_ID = 0x1967;
+// for (let x = 0; x < 8; x++) {
+//   for (let y = 0; y < 8; y++) {
+//     push2.setColor([x + 1, y + 1], 160 + x + 1 + y + 1);
+//   }
+// }
 
-console.log('Hello World!');
+const canvas = Canvas.createCanvas(960, 160);
+const ctx = canvas.getContext('2d');
+
+const PI = Math.PI;
+
+const state = {
+  cc1: 100,
+};
+
+function drawFrame(ctx) {
+  // const value = frameNum;
+  const value = Math.min(state.cc1, 127);
+
+  ctx.strokeStyle = '#ff0';
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, 960, 160);
+  ctx.lineWidth = 3;
+
+  const start = 0 - PI - PI / 4;
+  const end = start + (PI * 2 - PI / 2);
+  const angleMax = end - start;
+
+  const midiValue = Math.floor(value % 128);
+  // const midiValue = 100;
+
+  const ratio = midiValue / 127;
+  // const ratio = 127 / 127;
+  const pos = angleMax * ratio;
+
+  ctx.beginPath();
+  const x = 60;
+  const y = 130;
+  const size = 20;
+
+  ctx.arc(x, y, size, start, start + pos);
+  ctx.lineTo(x, y);
+  ctx.stroke();
+
+  ctx.font = '100 18px "SF Pro Display"';
+  ctx.fillStyle = '#fff';
+
+  let xPos = 55;
+  if (midiValue >= 10 && midiValue <= 99) {
+    xPos = 50;
+  } else if (midiValue >= 100) {
+    xPos = 45;
+  }
+  const yPos = 100;
+  // const xPos = midiValue >= 100 ? 45 : 48;
+  // ctx.fillText(`${midiValue}`, xPos, 100);
+  ctx.fillText(`${midiValue}`, xPos, yPos);
+}
+
+// let frameNum = 0;
+
+function nextFrame() {
+  // drawFrame(ctx, frameNum);
+  // frameNum++;
+  drawFrame(ctx);
+  sendFrame(ctx, function (error) {
+    // we can ignore any error here, more or less
+    setTimeout(nextFrame, 15); // Do not use nextTick here, as this will not allow USB callbacks to fire.
+  });
+}
+
+let push2 = null;
+
+function closePush() {
+  console.log('=> Push disconnected!');
+  push2 && push2.close();
+  push2 = null;
+}
+
+initPush(function (error) {
+  if (error) {
+    console.log('Warning [init]: ', error.message);
+  } else {
+    console.log('=> Push connected!');
+    push2 = new Push2();
+  }
+
+  // It's ok to continue, as a Push 2 device may appear later
+  nextFrame();
+}, closePush);
+
+process.on('SIGINT', () => {
+  // for (let x = 0; x < 8; x++) {
+  //   for (let y = 0; y < 8; y++) {
+  //     push2.setColor([x + 1, y + 1], 0);
+  //   }
+  // }
+
+  closePush();
+  process.exit();
+});
